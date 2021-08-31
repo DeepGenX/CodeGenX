fa145dcf9545e9946c538528adffd6e855b8e6d2
Aryagm <80019072+Aryagm@users.noreply.github.com>
2021-08-31 19:38:09 -0400

    ✨Added option to allow user to edit model parameters

--- src/extension.ts
+++ src/extension.ts
@@ -9,15 +9,9 @@ export function activate(context: vscode.ExtensionContext) {
 
 	var out_lst = ['null']; 
 
-	// The parameters given to the model
-	const token_max_length = 512;
-	const temp = 1.22;
-	const top_p = 0.6;
-	const top_k = 40;
-	const stop_sequence = "<|endoftext|>";
 
 	// Function to get response from GPT-J
-	const getOutput = async function(input: string) {
+	const getOutput = async function(input: string, token_max_length: number, temp: number, top_p: number, top_k: number,stop_sequence: string) {
 		const payload = { 'context': input, 'token_max_length': token_max_length, 'temperature': temp, 'top_p': top_p, 'top_k': top_k, 'stop_sequence':stop_sequence};
 		const result = await axios.post(`http://api.vicgalle.net:5000/generate`, null, { params: payload });
 		return result.data.text;
@@ -62,12 +56,23 @@ export function activate(context: vscode.ExtensionContext) {
 			var cursorPosition = editor.selection.active;
 			var input = editor.document.getText(new vscode.Range(0, 0, cursorPosition.line, cursorPosition.character));
 			
+			// Getting the settings:
+			const currentDocument = editor.document;
+			const configuration = vscode.workspace.getConfiguration('', currentDocument.uri);
+
+			const temp = Number(configuration.get('Codegenx.Temperature', {}));
+			const top_p = Number(configuration.get('Codegenx.Top_P', {}));
+			const top_k = Number(configuration.get('Codegenx.Top_K', {}));
+			const max_length = Number(configuration.get('Codegenx.MaxLength', {}));
+			const stop_sequence = String(configuration.get('Codegenx.StopSequence', {}));
+
+
 			var output = null;
 
 			const filename = editor.document.uri.fsPath;
 			const extension = 	filename.split('.').slice(-1)[0];
 			var comment = comment_map[extension];
-			
+
 			if (extension=="py"){
 				var temp_input = input.trim();
 				var last_line = temp_input.split(/\r?\n/).slice(-1)[0].trim();
@@ -99,7 +104,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 			// If the output is null then run the model again:
 			if (output==null){
-			var out = await getOutput(input.trim());
+			var out = await getOutput(input.trim(), max_length, top_p, temp, top_k, stop_sequence);
 			out = out.trim();
 
 			// Choose the appropriate comment symbol:
