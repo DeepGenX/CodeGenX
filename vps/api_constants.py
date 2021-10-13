c0a72d2a02dc63f3ac652e2e66722d5f5eae28bd
Matthias <48327140+Matthias1590@users.noreply.github.com>
2021-10-13 20:39:51 +0200

    Moved REQUESTS_PER_MINUTE into api_constants.py

--- vps/api_constants.py
+++ vps/api_constants.py
@@ -1,2 +1,3 @@
-ALLOW_REGISTRATION = True
+REQUESTS_PER_MINUTE = 5 # Users can make 5 requests per minute
+ALLOW_REGISTRATION = True # New users are allowed to register
 VERIFY_TIME = 15 * 60 # Give the user 15 minutes to verify
